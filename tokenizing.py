645da89a38dd56150aa9fa397ca2ea470aa3f533
Kayla Perez <perezkay@uw.edu>
2022-05-05 21:49:10 -0700

    revamped cleaning method

--- tokenizing.py
+++ tokenizing.py
@@ -25,10 +25,51 @@ sentence_tokens: These are the clean sentences that have had all of their stop
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
+from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
 import re
 
+def get_raw_sentences(pdf_path):
+    punkt_param = PunktParameters()
+    punkt_param.abbrev_types = set(['dr', 'vs', 'mr', 'mrs', 'prof', 'inc', 'al', 'etc', 'e.g', 'i.e', 'fig'])
+    sentence_splitter = PunktSentenceTokenizer(punkt_param)
+    with open(pdf_path, "r") as file:
+            # returns one string with all of the text
+        raw_text = file.read()
+    file.close()
 
-def get_clean_sentences(file_path):
+    raw_sentences = sentence_splitter.tokenize(re.sub(" +", " ", raw_text))
+    return raw_sentences
+
+def get_clean_line(sentence):
+    # removes any instance of an in-text citation following any of these formats:
+    # (Smith & Johnson, 2019), (Smith, 2019), (Smith et al., 2019), (Smith & Johnson, 2019; James, 2019)
+    clean_text = re.sub(r"\s\((?:(?:[\w \.&]+\, )+[0-9]{4}[;|:]*\s*)+\)", "", sentence)
+
+    # need to remove instances of citations within sentences (e.g "Smith et al. (2018) said that....")
+    # as these cause the sentences to get split up where they aren't supposed to
+    # clean_text = re.sub("(et al\.)", "et al", clean_text)
+    # clean_text = re.sub("(e\.g\.)", "e.g", clean_text)
+    # clean_text = re.sub("(i\.e\.)", "i.e", clean_text)
+    # clean_text = re.sub("(etc\.)", "etc", clean_text)
+    # clean_text = re.sub("(Fig\.)", "Fig", clean_text)
+    clean_text = re.sub("(Table \w+)", "Table", clean_text)
+
+
+    # also need to remove numbers, includes decimals
+    clean_text = re.sub(r"\d+(\.[0-9]+)*", "", clean_text)
+    clean_text = re.sub(r"{.+}@\S+", "", clean_text)
+
+
+    # removes any URLs
+    #clean_text = re.sub(r"http\S+", "", clean_text)
+    clean_text = re.sub(r'(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#“”"-]*[\w@?^=%&\/~+“”"#-])', " ", clean_text)
+    clean_text = re.sub(r"www\.\S+", "", clean_text)
+
+    # removes any additional white space (e.g: "I like      cats   .") 
+    clean_text = re.sub(" +", " ", clean_text)
+    return clean_text
+
+def get_clean_sentences(raw_sentences):
     """
     Cleans pre-processed PDF text to remove any noise and converts the text
     into a list of sentences for further processing.
@@ -44,41 +85,14 @@ def get_clean_sentences(file_path):
         A list of all of the sentences within the text
     """
 
-    with open(file_path, "r") as file:
-        # returns one string with all of the text
-        raw_text = file.read()
-    file.close()
+    clean_sentences = []
+    for sentence in raw_sentences:
+        clean_sentences.append(get_clean_line(sentence))
+    return clean_sentences
 
-    # this removes any instance of [<number>] as some papers use this as a
-    # way of citing
-    clean_text = re.sub(r"\[\d+\]", "", raw_text)
-
-    # removes any instance of in-text citations following any of these formats:
-    # (Smith & Johnson, 2019), (Smith, 2019), (Smith et al., 2019),
-    # (Smith & Johnson, 2019; James, 2019)
-    clean_text = re.sub(r"\((?:(?:[\w \.&]+\, )+[0-9]{4}[;|:]*\s*)+\)", "", clean_text)
-
-    # need to remove instances of citations within sentences 
-    # (e.g "Smith et al. (2018) said that....")
-    # as these cause the sentences to get split up where they aren't supposed to
-    clean_text = re.sub("(et al.)", "et al", clean_text)
-
-    # removes numbers, includes decimals
-    clean_text = re.sub(r"\d+\.*", " ", clean_text)
-
-    # removes any URLs
-    clean_text = re.sub(r"http\S+", "", clean_text)
-    clean_text = re.sub(r"www\.\S+", "", clean_text)
-
-    # removes any additional white space (e.g: "I like      cats   .") 
-    clean_text = re.sub(" +", " ", clean_text)
-    
-    sentences = sent_tokenize(clean_text)
-    return sentences
-
-def get_tokenized_sentences(sentences):
+def get_tokenized_sentences(raw_sentences):
     # remove punctuation and make all letters lowercase
-    clean_sentences = [re.sub(r'[^\w\s]','',sentence.lower()) for sentence in sentences]
+    clean_sentences = [re.sub(r'[^\w\s]','',sentence.lower()) for sentence in get_clean_sentences(raw_sentences)]
 
     stop_words = stopwords.words('english')
 
@@ -87,3 +101,7 @@ def get_tokenized_sentences(sentences):
     sentence_tokens = [[words for words in word_tokenize(sentence) if words not in 
                         stop_words] for sentence in clean_sentences]
     return sentence_tokens
+
+
+
+
